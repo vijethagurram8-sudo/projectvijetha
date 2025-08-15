e26de700f0f1a4e38ae2926e6a8d8cfcdfd0f223
SANDHIYA <SANDHIYA@DESKTOP-IRHS2JI>
2025-08-15 20:08:25 +0530

    Written and updated Succesfully

--- vijetha123/src/test/java/contacttest/ToCreatecontactTest.java
+++ vijetha123/src/test/java/contacttest/ToCreatecontactTest.java
@@ -5,7 +5,7 @@ public class ToCreatecontactTest {
 	public static void main(String[] args) {
 		System.out.println("-----succusfull------");
 		System.out.println("----vij-------");
-
+		System.out.println("sandhiya updated successfully");
 	}
 
 }
