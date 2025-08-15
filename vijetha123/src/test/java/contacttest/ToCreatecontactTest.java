7f5642c1ae2d4ac1da8d58463a9653ec1b6eca27
gurra <gurra@gurramprasunsai>
2025-08-15 18:50:38 +0530

    sending.......second

--- vijetha123/src/test/java/contacttest/ToCreatecontactTest.java
+++ vijetha123/src/test/java/contacttest/ToCreatecontactTest.java
@@ -4,6 +4,7 @@ public class ToCreatecontactTest {
 
 	public static void main(String[] args) {
 		System.out.println("-----succusfull------");
+		System.out.println("----vij-------");
 
 	}
 
--- vijetha123/src/test/java/oganizationstest/ToCreateOrganizationTest.java
+++ vijetha123/src/test/java/oganizationstest/ToCreateOrganizationTest.java
@@ -4,6 +4,7 @@ public class ToCreateOrganizationTest {
 
 	public static void main(String[] args) {
 		System.out.println("----succusfull--------");
+		System.out.println("----vij-------");
 
 
 	}
